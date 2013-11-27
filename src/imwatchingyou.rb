85c20fe224906bfe42d84322dd101ff6a4a0fa28
Victor Bersy <victorbersy+github@gmail.com>
2013-11-28 00:29:23 +0100

    add libs

--- src/imwatchingyou.rb
+++ src/imwatchingyou.rb
@@ -2,15 +2,23 @@ $: << File.dirname(__FILE__)
 
 require 'pp'
 require 'yaml'
+require 'rainbow'
+require 'twitter'
 
 require 'ImWatchingYou/CLI/Detector'
 require 'ImWatchingYou/CLI/Help'
 require 'ImWatchingYou/CLI/Config'
 
+require 'ImWatchingYou/Twitter/Stream'
+
+require 'ImWatchingYou/Display/Viewer'
+
+require 'ImWatchingYou/Launcher'
+
 module ImWatchingYou
   unless ARGV.first.nil?
     Detector.new(ARGV)
   else
-    pp "Launcher"
+    Launcher::start
   end
 end
