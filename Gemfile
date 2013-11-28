c3c22f65983cd1119ca7f9baaab50b951826ede8
Victor Bersy <victorbersy+github@gmail.com>
2013-11-28 16:40:31 +0100

    add mongo

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source 'https://rubygems.org' 
 gem "twitter", "~> 5.x.x"
 gem "rainbow", "~> 1.x.x"
+gem "mongo", "~> 1.9.2"
--- src/imwatchingyou.rb
+++ src/imwatchingyou.rb
@@ -3,16 +3,15 @@ $: << File.dirname(__FILE__)
 require 'pp'
 require 'yaml'
 require 'rainbow'
+require 'mongo'
 require 'twitter'
 
 require 'ImWatchingYou/CLI/Detector'
 require 'ImWatchingYou/CLI/Help'
 require 'ImWatchingYou/CLI/Config'
-
 require 'ImWatchingYou/Twitter/Stream'
-
+require 'ImWatchingYou/Twitter/Handler'
 require 'ImWatchingYou/Display/Viewer'
-
 require 'ImWatchingYou/Launcher'
 
 module ImWatchingYou
